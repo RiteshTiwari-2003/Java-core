73f22f6a6182ac96ed1ad42959e76cfbb017bdc6
Ritesh <riteshtiwari3112003@gmail.com>
2025-01-26 19:20:22 +0530

    this is my operator in java

---
+++ datatype.java
@@ -0,0 +1,37 @@
+/*data types in java
+ * 
+ * 1. primitive data types
+ * 2. non -primitive data types
+ * 
+ * the primitive data types include boolean , char ,byte, short, int, long , float, and double.
+ * the non primitive data types: the non primitive data types include Classes, Interfaces and Arrays.
+ * 
+ * 
+boolean data type:  boolean have values like false and true , and default size of boolean is 1 bit.
+char datatype : default alue of char is '\u0000' and default size of char datatype is 2 byte.
+byte datatype: default value of byte data type is 0 and default size of byte datatype is 1 byte,
+short datatype: default value of short datatype is o and default size of short datatype is 2 byte.
+int datatype: default value of int datatype is 0, and default size of int datatype is 4 byte.
+long datatype:default value of long datatype is 0L, and default size of long datatype is 8 byte,
+float datatype; default value of float datatype is 0.0f , and default size of float datatype is 4 byte.
+double datatype: default value of double datatype is 0.0d, and size is 8 byte
+
+
+why char uses 2 bytes in java and what is '\u0000'?
+it is because java uses unicode system not ascii value , the \u0000 is the lowest value of unicodesystem, 
+
+what is the unicode system?
+unicode is the universal international standard character encoding that is capable of representing most of the world's written language.
+
+why java uses unicoe system?
+before unicode , there were many language standard :
+
+ASCII (American Standard Code for Information Interchange) for the United States.
+ISO 8859-1 for Western European Language.
+KOI-8 for Russian.
+GB18030 and BIG-5 for chinese, and so on.
+
+operator in java:
+
+
+ */
