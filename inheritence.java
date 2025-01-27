d5b3ac34de4076743065a681ee7532e5608703bc
Ritesh <riteshtiwari3112003@gmail.com>
2025-01-28 01:24:54 +0530

    this is inheritense commiit

---
+++ inheritence.java
@@ -0,0 +1,16 @@
+/*
+ * inheritense in java
+ * 
+ * inheritense in java is mechanism in which one object acquire all properties and behaviour of parent object .
+ * it is importent part of oops.
+ * 
+ * the idea behind inheritense in java is that we can create new classes that are build upon existing class.when we inherit methods from an existing classeswe can reuse method and 
+ * fields of parent classes.
+ * 
+ * inheritense in java enable a class to inherit properties and action from another class called a super class and parent class ,
+ * a class derived from super class is called child class and subclass.
+ * 
+ * inheritence represent is a relation ship which is also known as parent child relation ship.
+ * 
+ * 
+ */
