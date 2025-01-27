6bc369610058e43626934ea9119de4b6d843d025
Ritesh <riteshtiwari3112003@gmail.com>
2025-01-27 15:44:38 +0530

    this is my overloadng commit

---
+++ methodoverload.java
@@ -0,0 +1,7 @@
+/*
+ * method overloading in java
+ * 
+ * method overloading occur within the same class and allow you to define multiple method with the same name but different parameters.the 
+ * compiler differentiate between these methods based on number, type and order of their parameter.
+ * 
+ */
