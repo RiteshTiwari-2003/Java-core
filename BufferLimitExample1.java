7607dacfce0e84a058841784d78fcc8e4d59abe8
Ritesh <riteshtiwari3112003@gmail.com>
2025-01-27 13:29:14 +0530

    this is java se 45 commit

--- BufferLimitExample1.java
+++ BufferLimitExample1.java
@@ -21,7 +21,17 @@ public class BufferLimitExample1{
         db.limit(2);
 
 
-
+    
     }}
+
+    /*java short keyword
+     * the short keyword default size is -32768 to 32767, its default value is 0 , its default size is 2 bytes, it is used to save memory in large array
+     * 
+     * java long keyword:
+     * java long keyword is a primitive data type , it is used to declare a vaiable , it can also be used with methods,it can hold the 64 bits two's compliment integer.
+     * the long containe minimum value of -2^63 and maximum value 2^63-1
+     * 
+     * from java 8, the long can represent as an unsigned 64bit long , which has a minimum value of 0 and and a maximum value is 2^64-1
+     */
 
     
