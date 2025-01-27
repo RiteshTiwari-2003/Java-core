31f465ab64a6474f5c10ae2767892618b50287ff
Ritesh <riteshtiwari3112003@gmail.com>
2025-01-27 15:50:51 +0530

    this is method overriding commit

--- methodoverload.java
+++ methodoverload.java
@@ -4,4 +4,20 @@
  * method overloading occur within the same class and allow you to define multiple method with the same name but different parameters.the 
  * compiler differentiate between these methods based on number, type and order of their parameter.
  * 
+ * key point:
+ * same name but different parameterlist
+ * it is compiler time polymorphism.
+ * method can have different return type, but method signature must differ.
+ * 
+ * 2. method overriding:
+ * 
+ * method overriding occur between a parent and child class, where a method in child class has the same name , return type and parameter as mehod in parent class.
+ * 
+ * key points:
+ * same method name, same parameter and same return type.
+ * it is run time (dynamic ) polymorphism .
+ * the @override annotation is used to indicate that a method is being overridden.
+ * 
+ * 
+ * 
  */
