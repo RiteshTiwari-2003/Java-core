6d8c984579bc4a499c044071077a3d338417abb0
Ritesh <riteshtiwari3112003@gmail.com>
2025-01-28 22:20:00 +0530

    this is mmy wrapper class commit

--- javapackage.java
+++ javapackage.java
@@ -3,5 +3,9 @@
  * 
  * java package is the group of  similer types of class and interfaces and subpackage
  * 
+ * what is the wrapper class in java
  * 
+ * the wrapper class in java provide the mechanism to convert primitive into object and object into primitive .
+ * autoboxing and unboxing feature convert primitive into object and object into primitive automatically. the automatic conversion of 
+ * primitive into object and object into primitive is called autoboxung and unboxing.
  */
